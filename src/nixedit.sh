3140862be4c07cb59285aea440372ebc3b4e90c8
nixedit <miyu@allthingslinux.com>
2024-09-19 07:01:47 -0500

    change default operation

--- src/nixedit.sh
+++ src/nixedit.sh
@@ -21,7 +21,7 @@ update() {
 }
 
 update_system() {
-  task_with_timer "updating pacakges database" "nix-channel --update > /dev/null" "fatal" "failed to update package database." "updating database complete. "
+  task_with_timer "updating pacakges database" "nix-channel --update > /dev/null" "fff" "failed to update package database." "updating database complete. "
 }
 
 update_search() {
