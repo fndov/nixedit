0af2c44d08c23566d1aa9dd294cfc5e61b52a684
miyu <tommybice1@gmail.com>
2024-09-28 07:51:51 -0500

    TUI Option

--- src/nixedit.sh
+++ src/nixedit.sh
@@ -109,6 +109,7 @@ EOF
 }
 
 default_operation() {
+  update_package_age > /dev/null
   if ! sudo true; then
     exit 1
   fi
@@ -240,12 +241,40 @@ github() {
 
 delete() {
   sudo true
-  task_with_timer "deleting old packages" "sudo nix-collect-garbage --delete-older-than 1d" "error" "failed to delete packages" "deletion complete"
+  
+  if [ -z "$2" ]; then
+    days=$(<"$HOME/.cache/nixedit/package-age.txt")
+  elif [[ "$2" =~ ^[0-9]+$ ]]; then
+    days="$2"
+  else
+    echo "Usage: nixedit --delete [num] 1-100 days old, default 1"
+    return 1
+  fi
+  task_with_timer "deleting old packages" "sudo nix-collect-garbage --delete-older-than ${days}d" "error" "failed to delete packages" "deletion complete"
+}
+
+
+update_package_age() {
+    local CACHE_DIR="$HOME/.cache/nixedit"
+    local PACKAGE_AGE_FILE="$CACHE_DIR/package-age.txt"
+
+    mkdir -p "$CACHE_DIR"
+
+    if [[ ! -f "$PACKAGE_AGE_FILE" ]]; then
+        echo "1" > "$PACKAGE_AGE_FILE"
+        echo "Created $PACKAGE_AGE_FILE with initial value 1."
+    else
+        local CURRENT_AGE
+        CURRENT_AGE=$(<"$PACKAGE_AGE_FILE") # Read the current age
+        CURRENT_AGE=$((CURRENT_AGE + 1))     # Increment the age
+        echo "$CURRENT_AGE" > "$PACKAGE_AGE_FILE" # Write the new age
+        echo "Updated $PACKAGE_AGE_FILE to value $CURRENT_AGE."
+    fi
 }
 
 debug() {
   rm -rf ~/.cache/nixedit > /dev/null 2>&1
-  rm -rf ~/.nixedit/Home/ ~/.nixedit/Configuration/ ~/.nixedit/Flake/
+  rm -rf ~/.nixedit/Home/ ~/.nixedit/Configuration/ ~/.nixedit/Flake/ > /dev/null 2>&1
   echo "debug: nixedit reset complete."
 }
 
@@ -459,8 +488,303 @@ uninstall() {
   task_with_timer "uninstalling $PACKAGE" "sudo nixos-rebuild switch" "error" "uninstall failed" "uninstalled $PACKAGE"
 }
 
+tui() {
+update_package_age > /dev/null
+    if ! sudo true; then
+    exit 1
+  fi
+  VAR=$(dialog --title "Nixedit" \
+    --menu "Select system operation:" 0 0 0 \
+      1 "Search packages" \
+      2 "Help" \
+      3 "Connect Github" \
+      4 "Configuration" \
+      5 "Backup computer" \
+      6 "Install software" \
+      7 "Uninstall software" \
+      8 "List restore points" \
+      9 "Delete restore points" \
+      10 "Optimise storage" \
+      11 "Rebuild & Reboot" \
+      3>&1 1>&2 2>&3)
+
+  case $VAR in
+    1)
+     USER_INPUT=$(dialog --title "NixPKG Search" --inputbox "Name the package you're looking for" 8 40 3>&1 1>&2 2>&3 3>&-)
+      
+      if [[ -z "$USER_INPUT" ]]; then
+          tui; exit 0
+      fi
+      
+      tui_search=$(nix search nixpkgs | grep "$USER_INPUT" | awk -F "legacyPackages.x86_64-linux." '{print $2}' | awk '{print $1}' | sed 's/\..*//' | sort -u | grep -v '^$' | grep "^$USER_INPUT" | sed 's/\x1B\[[0-9;]*[a-zA-Z]//g')
+      
+      package_array=()
+      menu_items=""
+      i=1
+      
+      if [ -z "$tui_search" ]; then
+        dialog --title "NixPKG Install" --msgbox "No results found for '$USER_INPUT'" 6 40
+        tui; exit 0
+      fi
+      
+      for item in $tui_search; do
+        menu_items+="$i $item "
+        package_array+=("$item")
+        i=$((i+1))
+      done
+      
+      selected=$(dialog --title "NixPKG Search" --menu "Showing results for: $USER_INPUT" 0 0 0 $menu_items 3>&1 1>&2 2>&3)
+      
+      if [ $? -eq 1 ]; then
+        tui; exit 0
+      fi
+      
+      selected_package="${package_array[$((selected-1))]}"
+      
+      dialog --title "NixPKG Search" --yes-label "Install" --no-label "Cancel" --extra-button --extra-label "Add Package" --yesnbao "\nWhat would you like to do with this package?\n\nSelected: $selected_package" 9 54
+      
+      case $? in
+          0)
+            dialog --title "NixPKG Install" --infobox "Your system is currently building.\n\nInstalling: $selected_package" 6 40
+            output=$(install --install "$selected_package")
+            if echo "$output" | grep -q "installed"; then
+              dialog --title "NixPKG Install" --msgbox "Your system successfully built.\nInstalled: $selected_package" 7 40
+            elif echo "$output" | grep -q "usage"; then
+              true
+            elif echo "$output" | grep -q "exist"; then
+              dialog --title "NixPKG Install" --msgbox "Your system failed to build.\nUnable to install: '$selected_package'\n\n            Package may not exist." 8 50
+            elif echo "$output" | grep -q "already"; then
+              dialog --title "NixPKG Install" --msgbox "Your system failed to build.\nUnable to install: $selected_package\n\n        Already present package list." 8 50
+            fi
+            tui
+            exit 0
+            ;;
+          1)  
+            tui
+            exit 0
+            ;;
+          3)  
+            output=$(add --add "$selected_package")
+            if echo "$output" | grep -q "added"; then
+              dialog --title "NixPKG Search" --msgbox "         Added package to Configuration.\n\n              pending next build." 7 55
+            elif echo "$output" | grep -q "usage"; then
+              true
+            elif echo "$output" | grep -q "error"; then
+              dialog --title "NixPKG Search" --msgbox "       Added package to Configuration.\n\n              pending next build." 7 50
+            elif echo "$output" | grep -q "present"; then
+              dialog --title "NixPKG Search" --msgbox "             Unable to add package.\n\n       Name already found in package list." 7 53
+            fi
+            tui
+            exit 0
+            ;;
+      esac
+      ;;
+    2)
+      # Action for "Help"
+      dialog --title "Nixedit Help." --msgbox "TUI Usage: Designed to be user friendly,\n\nIf there's an issue with the TUI please submit an isssue to https://github.com/fndov/nixedit.\n\nCLI Usage: NixOS build automating utility, for your Configuration & System. Streamlined process.       \n\nSetup commands:       \n  --github        Connect your dedicated github repository to store backups.       \n         \nSingular options:       \n  --help          Show this help message and exit       \n  --version       Display current nixedit version       \n         \n  --search        Search packages       \n  --config        Open configuration       \n  --add           Add package to configuration       \n  --remove        Remove package from configuration       \n  --install       Install package to system       \n  --uninstall     Uninstall package from system       \n  --upload        Upload configuration       \n  --update        Update the nixpkgs & search, databases       \n  --rebuild       Rebuild system       \n  --list          List pervious generations       \n  --delete        Delete older packages       \n  --optimise      Optimize Nix storage       \n  --graph         Browse dependency graph       \n  --find          Find local packages       \n               \nIf no option is provided, the default operation will:       \n  - Perform a search       \n  - Open the configuration file for editing       \n  - Update system packages       \n  - Rebuild the system       \n  - Upload configuration to repository       \n  - Delete old packages       \n  - Optimise package storage\n\n\n                                    Experiments unsupported." 0 0
+      tui; exit 0
+      ;;
+    3)
+      # Action for "Connect Github"
+      repo=$(dialog --title "Connect GitHub" --inputbox "Link repository for configuration storage.\n\nVisit this link and create a dedicated repository.\n https://github.com/new\n\n Every upload will push here" 14 60 3>&1 1>&2 2>&3)
+
+      if [ -z "$repo" ]; then
+        tui; exit 0
+      fi
+      
+      mkdir ~/.nixedit/ > /dev/null 2>&1 
+      mkdir ~/.nixedit/Configuration/ > /dev/null 2>&1 
+      mkdir ~/.nixedit/Flake/ > /dev/null 2>&1 
+      mkdir ~/.nixedit/Home/ > /dev/null 2>&1 
+    
+      rm -rf ~/.nixedit/.git > /dev/null 2>&1 
+      cd ~/.nixedit/ > /dev/null 2>&1 
+      cp -f /etc/nixos/configuration.nix ~/.nixedit/Configuration/configuration.nix-$(date +%m-%d-%H:%M)
+      
+      git init > /dev/null 2>&1
+      git config --global user.name "nixedit" > /dev/null 2>&1
+      git config --global user.email "miyu@allthingslinux.com" > /dev/null 2>&1
+      git add . > /dev/null 2>&1
+      git commit -m "NixOS Backup" > /dev/null 2>&1
+      
+      git remote add origin "$repo" > /dev/null 2>&1
+      git checkout -b main > /dev/null 2>&1
+      git checkout main origin/main > /dev/null 2>&1
+      
+      output=$(git push -u origin main --force 2>&1)
+      if echo "$output" | grep -q "branch 'main' set up to track 'origin/main'"; then
+        dialog --title "Connect GitHub" --msgbox "\n   Configuration Synced with repository.\n\n   $repo" 9 47
+        tui; exit 0
+      else
+        dialog --title "Connect GitHub" --msgbox "\n  Sync failed, Check URL or token settings.\n\n   $repo" 9 49
+        tui; exit 0
+        fi
+      ;;
+    4)
+      # Action for "Configuration"
+      if 
+      sudo -S dialog --editbox /etc/nixos/configuration.nix 0 0
+      then
+        dialog --infobox "Saved changes, pending next build." 4 40
+      sleep 3
+      else
+        dialog --infobox "Changes canceled, nothing new to build." 4 43
+      sleep 3
+      fi
+      tui; exit 0 # Return
+      ;;
+    5)
+      # Action for "Backup computer"
+      dialog --infobox "Uploading configuration to GitHub..." 4 40
+      output=$(upload)
+      if echo "$output" | grep -q "complete"; then
+          message="Upload successful!"
+      elif echo "$output" | grep -q "failed"; then
+          message="Upload failed."
+      else
+          message="Upload status unknown. No clear indication of success or failure."
+      fi
+      dialog --msgbox "$message" 5 40
+      tui; exit 0
+      ;;
+    6)
+      # Action for "Install software"
+      USER_INPUT=$(dialog --title "NixPKG Install" --inputbox "Name the package you're trying to install" 8 45 3>&1 1>&2 2>&3 3>&-)
+      
+      dialog --title "NixPKG Install" --infobox "Your system is currently building.\nInstalling: $USER_INPUT" 4 40
+
+      output=$(install --install $USER_INPUT)
+      if echo "$output" | grep -q "complete"; then
+          dialog --title "NixPKG Install" --msgbox "Your system successfully built.\nInstalled: $USER_INPUT" 7 40
+          tui; exit 0
+      elif echo "$output" | grep -q "usage"; then
+          tui; exit 0
+      elif echo "$output" | grep -q "exist"; then
+          dialog --title "NixPKG Install" --msgbox "Your system failed to build.\nUnable to install: '$USER_INPUT'\n\n            Package may not exist." 8 50
+          tui; exit 0
+      elif echo "$output" | grep -q "already"; then
+          dialog --title "NixPKG Install" --msgbox "Your system failed to build.\nUnable to install: $USER_INPUT\n\n        Already present package list." 8 50
+          tui ; exit 0
+       else
+          tui; exit 0
+      fi
+      ;;
+    7)
+      # Action for "Uninstall software"
+      USER_INPUT=$(dialog --title "NixPKG Uninstall" --inputbox "Name the package you're trying to uninstall" 8 47 3>&1 1>&2 2>&3 3>&-)
+      dialog --title "NixPKG Uninstall" --infobox "Uninstalling package '$USER_INPUT'. Your system is currently building..." 4 74
+
+      output=$(uninstall --uninstall $USER_INPUT)
+      if echo "$output" | grep -q "uninstalled"; then
+          dialog --title "NixPKG Uninstall" --msgbox "Successfully uninstalled '$USER_INPUT'." 5 40
+          tui; exit 0
+      elif echo "$output" | grep -q "usage"; then
+          tui; exit 0
+      elif echo "$output" | grep -q "list"; then
+          dialog --title "NixPKG Uninstall" --msgbox "Failed to uninstall '$USER_INPUT'. Not found in package list." 5 65
+          tui; exit 0
+       else
+          tui; exit 0
+      fi
+      ;;
+    8)
+      # Action for "List restore points"
+      output=""
+      while IFS= read -r line; do
+          output+="$line\n\n"  
+      done < <(list --list)
+      
+      dialog --title "List restore points" --msgbox "    Configurations can be found in\n        .nixedit/Configuration \n\n$output" 0 0
+      ;;
+    9)
+      # Action for "Delete restore points"
+      USER_INPUT=$(dialog --title "Delete restore points" --rangebox "Select the maximum age for restore points (1-30 days)" 9 57 1 30 7 3>&1 1>&2 2>&3 3>&-)
+      
+      if [[ -z "$USER_INPUT" ]]; then
+          tui; exit 0
+      fi
+      
+      if [[ "$USER_INPUT" -eq 1 ]]; then
+          message="Are you sure you want to delete all restore points older than $USER_INPUT day?"
+      else
+          message="Are you sure you want to delete all restore points older than $USER_INPUT days?"
+      fi
+      
+      dialog --title "Delete restore points" --yesno "$message" 7 40
+      
+      if [[ $? -ne 0 ]]; then
+          tui; exit 0
+      fi
+
+      echo "$USER_INPUT" > "~/.cache/nixedit/package-age.txt"
+
+      dialog --title "Delete restore points" --infobox "Deleting outdated restore points..." 4 39
+      output=$(delete --delete)
+      
+      if echo "$output" | grep -q "complete"; then
+          dialog --title "Delete restore points" --msgbox "Successfully deleted restore points" 6 40
+          tui; exit 0
+      elif echo "$output" | grep -q "usage"; then
+          tui; exit 0
+      elif echo "$output" | grep -q "failed"; then
+          dialog --title "Delete restore points" --msgbox "Failed to delete outdated restore points" 6 45
+          tui; exit 0
+      else
+          tui; exit 0
+      fi
+      ;;
+    10)
+      # Action for "Optimise storage"
+      dialog --title "Optimise storage" --infobox "\n  Currently working on system symlinks.\n\n  This may take 5-10 minutes." 8 50
+      output=$(optimise --optimise)
+
+      "error" "optimising has failed" "optimisation complete"
+
+      if echo "$output" | grep -q "complete"; then
+        dialog --title "Optimise storage" --msgbox "\n  System symlinks have been optimised.\n\n  Operation complete." 8 50
+        tui; exit 0
+      elif echo "$output" | grep -q "usage"; then
+        tui; exit 0
+      elif echo "$output" | grep -q "failed"; then
+        dialog --title "Optimise storage" --msgbox "\n  Failed to optimise system symlinks.\n\n  Unable to complete operation. Unknown error." 8 50
+      tui; exit 0
+      else
+          tui; exit 0
+      fi
+      ;;
+    11)
+      # Action for "Rebuild & Reboot"
+      dialog --title "Rebuild & Reboot" --infobox "\n  Rebuilding system, computer will automatically reboot when done. \n\n  This may take 1-3 minutes. depending if the kernel is compiling." 8 72
+      output=$(optimise --optimise)
+
+      if echo "$output" | grep -q "complete"; then
+        dialog --title "Rebuild & Reboot" --infobox "\n  Rebuild complete, computer will automatically reboot when done. \n\n  Last phases: Update Upload Delete Optimise | ~1 minute left." 8 72
+
+        update_package_age > /dev/null echo 1
+        update_system > /dev/null; echo 5
+        update_search > /dev/null; echo 6
+        upload > /dev/null; echo 8
+        delete > /dev/null; echo 9
+        optimise > /dev/null; echo 10
+
+        reboot
+        
+        tui; exit 0
+      elif echo "$output" | grep -q "error"; then
+        tui; exit 0
+      elif echo "$output" | grep -q "failed"; then
+        dialog --title "Rebuild & Reboot" --msgbox "\n  Rebuild failed, computer unable to proceed with reboot safely. \n\n  Check Network & Configuration." 8 72
+      tui; exit 0
+      else
+          tui; exit 0
+      fi
+      ;;
+esac
+clear
+}
+
 version() {
-  echo nixedit 0.85
+  echo nixedit 0.9
 }
 
 help() {
@@ -544,7 +868,7 @@ case "$1" in
     list
     ;;
   --delete)
-    delete
+    delete "$@"
     ;;
   --find)
     find "$@"
@@ -576,3 +900,4 @@ case "$1" in
     exit 1
     ;;
 esac
+
