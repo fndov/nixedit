bcf2b009ae6f82eb2c249593086be941e5eda152
miyu <tommybice1@gmail.com>
2024-10-13 00:14:22 -0500

    NEW Feature: Profiles

--- src/nixedit.sh
+++ src/nixedit.sh
@@ -1118,7 +1118,7 @@ Singular options: (some hame short options '"'-i'"')
   --rebuild       Rebuild system
   --profile       List existing profiles
   --generation    List existing generations
-  --delete        Delete older packages
+  --delete        Delete packages & profiles
   --optimise      Optimize Nix storage
   --graph         Browse dependency graph
   --find          Find local packages
